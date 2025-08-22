93b617475c82588bc516cbce79dc311965fe8b85
Yordanos Shanbel <yordanosshanbel28@gmail.com>
2025-08-22 16:14:31 -0300

    Enhance age calculation logic in Age Calculator; fix age month and day calculations, and update result display. Add margin to result span for better alignment.

--- script.js
+++ script.js
@@ -18,9 +18,45 @@ function validateDate() {
   let resultMonth = document.querySelector('.result-month');
   let resultYear = document.querySelector('.result-year');
 
+  // Variables to calculate the current date
   let today = new Date();
+
+  let currentYear = today.getFullYear();
+  let currentMonth = today.getMonth() + 1;
+  let currentDay = today.getDate();
+
+  // varibales to calculate birth date
+
   let birthDate = new Date(userYear, userMonth - 1, userDay);
 
-  console.log(birthDate);
+  let birthYear = birthDate.getFullYear();
+  let birthMonth = birthDate.getMonth() + 1;
+  let birthDay = birthDate.getDate();
 
+  // Calculate the age
+
+  let ageYear = currentYear - birthYear;
+  let ageMonth;
+  let ageDay;
+  
+
+  if(currentMonth >= birthMonth) {
+    (ageMonth = currentMonth - birthMonth);
+  } else {
+    ageYear--;
+    ageMonth = 12 + currentMonth - birthMonth;
+  }
+
+  if(currentDay >= birthDay) {
+    ageDay = currentDay - birthDay;
+  } else {
+    ageMonth--;
+    ageDay = 30 + currentDay - birthDay;
+  }
+
+
+
+  resultDay.innerHTML = ageDay;
+  resultMonth.innerHTML = ageMonth;
+  resultYear.innerHTML = ageYear;
 }
